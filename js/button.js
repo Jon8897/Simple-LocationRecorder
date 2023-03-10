5e34eb1d59e56ea729054bbe5054b0061671812b
Tom Draper <tomjdraper1@gmail.com>
2023-03-10 11:34:27 +0000

    Show and hide mobile nav bar close button

---
+++ js/button.js
@@ -0,0 +1,8 @@
+
+function hideCloseBtn() {
+    document.getElementById("nav-item-close").style.display = "none";
+}
+
+function showCloseBtn() {
+    document.getElementById("nav-item-close").style.display = "grid";
+}
